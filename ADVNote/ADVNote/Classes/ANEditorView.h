9274d2dbd7ba28ff0fac9d1e2b42179ef12d8c4a
youngwhan kim <clevekim@naver.com>
2012-10-29 00:43:59 +0900

    Editor View ADD.

---
+++ ADVNote/ADVNote/Classes/ANEditorView.h
@@ -0,0 +1,13 @@
+//
+//  ANEditorView.h
+//  ADVNote
+//
+//  Created by youngwhan kim on 12. 10. 29..
+//  Copyright (c) 2012년 youngwhan kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ANEditorView : UIView
+
+@end
---
+++ ADVNote/ADVNote/Classes/ANEditorView.m
@@ -0,0 +1,31 @@
+//
+//  ANEditorView.m
+//  ADVNote
+//
+//  Created by youngwhan kim on 12. 10. 29..
+//  Copyright (c) 2012년 youngwhan kim. All rights reserved.
+//
+
+#import "ANEditorView.h"
+
+@implementation ANEditorView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
